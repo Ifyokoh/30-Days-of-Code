b7e2ba22a195785770e5beea32ae946b0ec3778a
Ify <odibest1893@gmail.com>
2018-10-27 23:43:15 +0100

    Add Day 1

---
+++ Day 1 - Data Types.py
@@ -0,0 +1,25 @@
+# Task 
+# Complete the code in the editor below. The variables i, d and s are already declared and initialized for you. You must:
+
+# Declare 3 variables: one of type int, one of type double, and one of type String.
+# Read 3 lines of input from stdin (according to the sequence given in the Input Format section below) and initialize your 3 variables.
+# Use the + operator to perform the following operations: 
+# Print the sum of i plus your int variable on a new line.
+# Print the sum of d plus your double variable to a scale of one decimal place on a new line.
+# Concatenate s with the string you read as input and print the result on a new line.
+
+
+# Solution
+# Declare second integer, double, and String variables.
+new_i = int(input())
+new_d = float(input())
+new_s = input()
+# Read and save an integer, double, and String to your variables.
+
+# Print the sum of both integer variables on a new line.
+print(i + new_i)
+# Print the sum of the double variables on a new line.
+print(d + new_d)
+# Concatenate and print the String variables on a new line
+print(s + new_s)
+# The 's' variable above should be printed first.
